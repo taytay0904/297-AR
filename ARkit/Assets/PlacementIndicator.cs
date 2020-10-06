41caeeca655540fe16b726b6d74bf690db6b852c
Yixuan Yang <yixuan.yang@sjsu.edu>
2020-10-05 20:58:07 -0700

    Created the initial scene, PlacementIndicator, and spawn object
    functions:
    
    1.placement indicator follow and center
    2.placement indicator highlight
    3.plane finding
    4.spawn object on tap
    5.deploy to iOS
    
    Initial scene-setting:
    1.player setting
    2.iOS setting

---
+++ ARkit/Assets/PlacementIndicator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine;
+
+public class PlacementIndicator : MonoBehaviour
+{
+	 private ARRaycastManager rayManager;
+	 private GameObject visual; 
+     // Start is called before the first frame update
+     void Start()
+     {
+     	// get the components
+         rayManager = FindObjectOfType<ARRaycastManager>();
+         visual = transform.GetChild(0).gameObject;
+         
+		 // hide the placement indicator visual
+         visual.SetActive(false);
+        
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+		rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+		if(hits.Count > 0){
+			transform.position = hits[0].pose.position;
+			transform.rotation = hits[0].pose.rotation;
+
+			if(!visual.activeInHierarchy){
+				visual.SetActive(true);
+			}
+		}
+
+
+    }
+}
